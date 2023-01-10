21e0a26b7ccd3b128825d0b3b5ecb6e7cb60c769
Ramyadevi15 <ramyamugundhan1527@gmail.com>
2023-01-10 22:02:09 +0530

    new message code

---
+++ src/org/lang/LanguageInfo.java
@@ -0,0 +1,32 @@
+package org.lang;
+
+public class LanguageInfo {
+//method
+	//methodname 
+private void tamilLanguage() {
+	System.out.println("tamilLanguage is");
+}
+private void englishLanguage() {
+	System.out.println("englishlLanguage is");
+}
+private void hindiLanguage() {
+	System.out.println("hindiLanguage is");
+}
+private void teluguLanguage() {
+	System.out.println("teluguLanguage is");
+}
+//main 
+public static void main(String[] args) {
+	//object creation
+	//classname obj name = new classname();
+	LanguageInfo a=new LanguageInfo();
+	//methoid calling
+	a.tamilLanguage();
+	a.englishLanguage();
+	a.hindiLanguage();
+	a.teluguLanguage();
+	StateDetails b = new StateDetails();
+	b.southIndia();
+	b.northIndia();
+}
+}
---
+++ src/org/lang/StateDetails.java
@@ -0,0 +1,21 @@
+package org.lang;
+
+public class StateDetails {
+	//method
+	public void southIndia() {
+		//sysout
+		System.out.println("southIndia");
+	}
+	public void northIndia() {
+		//sysout
+		System.out.println("northIndia");
+	}
+	//main
+	public static void main(String[] agrs) {
+		StateDetails b = new StateDetails();
+		b.southIndia();
+		b.northIndia();
+	}
+
+
+}
